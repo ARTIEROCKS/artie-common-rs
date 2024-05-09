23fa572c6aafdad916d2724dd51733a76d313886
Luis Eduardo Imbernón <imbernon@gmail.com>
2024-05-09 11:33:34 +0200

    Update structure.rs
    Simplifying the uses of the rest of the project.

--- src/structure.rs
+++ src/structure.rs
@@ -2,3 +2,8 @@ pub mod field;
 pub mod input;
 pub mod block;
 pub mod workspace;
+
+pub use self::field::Field;
+pub use self::input::Input;
+pub use self::block::Block;
+pub use self::workspace::Workspace;
