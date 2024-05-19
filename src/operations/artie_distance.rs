3a56e62926b7022045087eef9333242b3d9c1a3e
Luis Eduardo Imbernón <imbernon@gmail.com>
2024-05-19 17:20:04 +0200

    Giving the blocks to be added, removed and repositioned in the workspace to be similar to the solution.

--- src/operations/artie_distance.rs
+++ src/operations/artie_distance.rs
@@ -1,5 +1,8 @@
+use crate::structure::hint::BlockPositionChange;
 use crate::structure::Block;
 use crate::structure::Workspace;
+use crate::structure::hint::WorkspaceAdjustments;
+use crate::structure::hint::BlockChange;
 use std::collections::HashSet;
 
 // Coefficients for weighting each distance type
@@ -14,6 +17,7 @@ pub struct ArtieDistance {
     pub position_distance: f64,
     pub input_distance: f64,
     pub total_distance: f64,
+    pub workspace_adjustments: WorkspaceAdjustments,
 }
 impl ArtieDistance {
     pub fn new() -> ArtieDistance {
@@ -23,6 +27,7 @@ impl ArtieDistance {
             position_distance: 0.0,
             input_distance: 0.0,
             total_distance: 0.0,
+            workspace_adjustments: WorkspaceAdjustments::new(),
         }
     }
 }
@@ -68,10 +73,16 @@ pub fn artie_distance(workspace: &Workspace, solution: &Workspace) -> ArtieDista
 
     // Calculate the block distance
     let common_blocks: HashSet<_> = workspace_block_names.intersection(&solution_block_names).cloned().collect();
-    let unique_blocks: HashSet<_> = workspace_block_names.symmetric_difference(&solution_block_names).cloned().collect();
+
+    // Unique blocks in workspace (to be removed)
+    let unique_workspace_blocks = workspace_block_names.difference(&solution_block_names).cloned().collect::<HashSet<_>>();
+
+    // Unique blocks in solution (to be added)
+    let unique_solution_blocks = solution_block_names.difference(&workspace_block_names).cloned().collect::<HashSet<_>>();
 
     // Set the distances for the ArtieDistance struct
-    artie_distance.block_distance += unique_blocks.len() as f64;
+    artie_distance.block_distance += unique_workspace_blocks.len() as f64;
+    artie_distance.block_distance += unique_solution_blocks.len() as f64;
 
 
     // 3- Position distance calculation
@@ -89,23 +100,66 @@ pub fn artie_distance(workspace: &Workspace, solution: &Workspace) -> ArtieDista
         collect_block_positions(block, &mut position, &mut solution_block_positions);
     }
 
+    // Add unique blocks from the workspace to blocks_to_remove
+    workspace.blocks.iter()
+        .filter(|block| unique_workspace_blocks.contains(&block.name))
+        .for_each(|block| {
+            artie_distance.workspace_adjustments.blocks_to_remove.push(BlockChange {
+                id: block.id.clone(),
+                name: block.name.clone(),
+            });
+        });
+
+    // Add unique blocks from the solution to blocks_to_add
+    solution.blocks.iter()
+        .filter(|block| unique_solution_blocks.contains(&block.name))
+        .for_each(|block| {
+            artie_distance.workspace_adjustments.blocks_to_add.push(BlockChange {
+                id: block.id.clone(),
+                name: block.name.clone(),
+            });
+        });
+
     // 3.2- Calculate the position distance for unique_blocks
     for block in &workspace_block_positions {
-        if unique_blocks.contains(&block.0) {
+        if unique_workspace_blocks.contains(&block.0) {
             artie_distance.position_distance += (block.1 as i32).abs() as f64;
         }
     }
     for block in &solution_block_positions {
-        if unique_blocks.contains(&block.0) {
+        if unique_solution_blocks.contains(&block.0) {
             artie_distance.position_distance += (block.1 as i32).abs() as f64;
         }
     }
 
     // 3.3- Calculate the position distance for common_blocks (blocks that are in the workspace and solution)
-    for block in workspace_block_positions {
-        if common_blocks.contains(&block.0) {
-            let solution_position = solution_block_positions.iter().find(|&x| x.0 == block.0).unwrap();
-            artie_distance.position_distance += (block.1 as i32 - solution_position.1 as i32).abs() as f64;
+    let mut solution_positions_remaining = solution_block_positions.clone();
+    for workspace_block in &workspace_block_positions {
+
+        if common_blocks.contains(&workspace_block.0) {
+            
+            // Checks if the block exists in the solution positions remaining, and gets the index and the element
+            if let Some((index, solution_block)) = solution_positions_remaining.iter().enumerate().find(|(_, block)| block.0 == workspace_block.0) {
+                // Calculate the position distance
+                artie_distance.position_distance += (workspace_block.1 as i32 - solution_block.1 as i32).abs() as f64;
+
+                // Adds the workspace block to reposition
+                artie_distance.workspace_adjustments.blocks_to_reposition.push(BlockPositionChange {
+                    block: BlockChange {
+                            id: workspace_block.0.clone(),
+                            name : workspace_block.0.clone(),
+                        },
+                    current_position: vec![workspace_block.1],
+                    target_position: vec![solution_block.1],
+                    }
+                );
+
+                // Remove the element from the solution positions remaining
+                solution_positions_remaining.swap_remove(index);
+            }else{
+                artie_distance.position_distance += workspace_block.1 as f64;
+            }
+            
         }
     }
 
@@ -169,30 +223,29 @@ pub fn collect_block_positions(block: &Block, position: &mut usize, block_positi
 
 // Helper function to calculate the input distance
 pub fn calculate_input_distance(workspace_block: &Block, solution_block: &Block, artie_distance: &mut ArtieDistance) {
-   
-   // Calculating the input distance
-   if workspace_block.name == solution_block.name {
-        for (workspace_field, solution_field) in workspace_block.fields.iter().zip(solution_block.fields.iter()) {
-            if workspace_field.name == solution_field.name {
-                if workspace_field.value != solution_field.value && workspace_field.is_numeric(){
+
+    if workspace_block.name == solution_block.name {
+        for workspace_field in &workspace_block.fields {
+            if let Some(solution_field) = solution_block.fields.iter().find(|f| f.name == workspace_field.name) {
+                if workspace_field.value != solution_field.value && workspace_field.is_numeric() && solution_field.is_numeric() {
                     artie_distance.input_distance += (workspace_field.value_as_double() - solution_field.value_as_double()).abs();
-                }else if workspace_field.value != solution_field.value {
+                } else if workspace_field.value != solution_field.value {
                     artie_distance.input_distance += 1.0;
                 }
             }
         }
     }
 
-    // Doing the calculations for nested blocks
-    for (workspace_nested_block, solution_nested_block) in workspace_block.nested.iter().zip(solution_block.nested.iter()) {
-        calculate_input_distance(workspace_nested_block, solution_nested_block, artie_distance);
+    // Recursively calculate the input distance for nested blocks
+    for workspace_nested_block in &workspace_block.nested {
+        if let Some(solution_nested_block) = solution_block.nested.iter().find(|b| b.name == workspace_nested_block.name) {
+            calculate_input_distance(workspace_nested_block, solution_nested_block, artie_distance);
+        }
     }
 
-    // Doing the calculations for the next block
-    if let Some(workspace_next_block) = &workspace_block.next {
-        if let Some(solution_next_block) = &solution_block.next {
-            calculate_input_distance(workspace_next_block, solution_next_block, artie_distance);
-        }
+    // Recursively calculate the input distance for the next block
+    if let (Some(workspace_next_block), Some(solution_next_block)) = (&workspace_block.next, &solution_block.next) {
+        calculate_input_distance(workspace_next_block, solution_next_block, artie_distance);
     }
 }
 
