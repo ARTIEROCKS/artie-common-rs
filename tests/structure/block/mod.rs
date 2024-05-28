7fc6d7aaaddaf6c8d0225feb959b37988fa5ae3b
Luis Eduardo Imbernón <imbernon@gmail.com>
2024-05-28 18:13:05 +0200

    Fixes in the unit tests.

--- tests/structure/block/mod.rs
+++ tests/structure/block/mod.rs
@@ -1,32 +1,35 @@
 use artie_common::structure::Block;
 use artie_common::structure::Field;
+use artie_common::structure::Input;
 
 #[test]
-pub fn test_add_field() {
-    let field_1 = Field::new("test", "123.45");
-    let field_2 = Field::new("test", "abc");
-    let mut block = Block::new("test", "test", "test", vec![field_1]);
-    block.add_field(field_2);
-    assert_eq!(block.fields.len(), 2);
+pub fn test_add_input() {
+    let input_1 = Input::new("test", "test", vec![Field::new("field_a", "123.45")]);
+    let input_2 = Input::new("test", "test", vec![Field::new("field_b", "abc")]);
+    let mut block = Block::new("test", "test", "test", vec![input_1]);
+    block.add_input(input_2);
+
+    assert_eq!(block.inputs.len(), 2);
 }
 
 #[test]
 pub fn test_add_nested() {
-    let field_1 = Field::new("test", "123.45");
+    let input_1 = Input::new("name", "code", vec![Field::new("field_a", "123.45")]);
     let mut block = Block::new("test", "test", "test", vec![]);
-    let nested_block = Block::new("test", "test", "test", vec![field_1]);
+    let nested_block = Block::new("test", "test", "test", vec![input_1]);
     block.add_nested(nested_block);
     assert_eq!(block.nested.len(), 1);
 }
 
 #[test]
 pub fn test_block_eq() {
-    let field_1 = Field::new("test", "123.45");
-    let field_2 = Field::new("test", "123.45");
-    let field_3 = Field::new("test", "abc");
-    let mut block_1 = Block::new("test", "test", "test", vec![field_1]);
-    let block_2 = Block::new("test", "test", "test", vec![field_2]);
+    let input_1 = Input::new("test", "test", vec![Field::new("field_a", "123.45")]);
+    let input_2 = Input::new("test", "test", vec![Field::new("field_b", "abc")]);
+    let input_3 = Input::new("test", "test", vec![Field::new("field_c", "123.45")]);
+
+    let mut block_1 = Block::new("test", "test", "test", vec![input_1]);
+    let block_2 = Block::new("test", "test", "test", vec![input_2]);
     assert_eq!(block_1, block_2);
-    block_1.add_field(field_3);
+    block_1.add_input(input_3);
     assert_ne!(block_1, block_2);
 }
